73bc4532e075aae6528f66f7ca6453deb177ccfc
ferlaron <ferlaron16@gmail.com>
2022-12-17 20:46:00 +0300

    circle area calculation

---
+++ Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Circle
+{
+    class Circle
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Введите радиус круга");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius,2);
+            System.Console.WriteLine("Площадь круга c радиусом {0} равна {1}",radius,area);
+        }
+    }
+}    
